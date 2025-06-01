a71965caca4c4511f9e9e3548424ffd0f1da0966
rafi542 <mdamirhussainrafi@gmail.com>
2025-06-01 12:17:33 +0600

    Create 41A - Translation.cpp

---
+++ 41A - Translation.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long int  i, n, g, a, b, c, t;
+  string s, y;
+  cin >> s;
+  cin >> y;
+  reverse(y.begin(), y.end());
+  if(s==y)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;  
+}  
