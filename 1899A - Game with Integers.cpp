590da29bd78e53ff06402f89df46e2a71d494c29
rafi542 <mdamirhussainrafi@gmail.com>
2025-06-20 11:16:33 +0600

    Create 1899A - Game with Integers.cpp

---
+++ 1899A - Game with Integers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long int g, i, j, h, cn, n, k, t, f, a, m;
+  long long int mid, left, right, s, sum, stop, sum1, start;
+  long long sum2;
+  vector<int>v;
+  vector<int>v1;
+  cin >> t;
+  while(t--)
+  {
+    cin >> n;
+    if(n%3 != 0)
+      cout << "First" << endl;
+    else
+      cout << "Second" << endl;  
+  }  
+}  
