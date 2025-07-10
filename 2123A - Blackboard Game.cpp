6b42573587290e6fe47f95fc62bf6ce95cebe0ab
rafi542 <mdamirhussainrafi@gmail.com>
2025-07-10 20:15:57 +0600

    Create 2123A - Blackboard Game.cpp

---
+++ 2123A - Blackboard Game.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long int t, i, j, k, g, sum, cn, h, n, excn;
+  long long int f, a, b, c, x, ar[950][950];
+  string s;
+  cin >> t;
+  while(t--)
+  {
+    cin >> n;
+    n = n - 1;
+    n = n - 3;
+    if(n%4 == 0)
+      cout << "Bob" << endl;
+    else
+      cout << "Alice" << endl;  
+  }
+}
+    
