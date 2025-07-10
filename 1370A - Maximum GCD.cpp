eb5cde66fcd6637ada62f732da157bd6d6dad085
rafi542 <mdamirhussainrafi@gmail.com>
2025-07-10 20:05:04 +0600

    Create 1370A - Maximum GCD.cpp

---
+++ 1370A - Maximum GCD.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int t, a, b, c, g, n;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        n /= 2;
+        cout << n << endl;
+    }
+}
+    
