29fd98645bca378e2ae6965a84701260a9b1f746
rafi542 <mdamirhussainrafi@gmail.com>
2025-07-11 09:09:28 +0600

    Create 2093A - Ideal Generator.cpp

---
+++ 2093A - Ideal Generator.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long int t, i, j, k, g, sum, cn, h, n, excn;
+  long long int f, a, b, c, x, ar[950][950];
+  string s;
+  cin >> t;
+  while(t--)
+  {
+    cin >> n;
+    if(n%2 != 0)
+      cout << "YES" << endl;
+    else
+      cout << "NO" << endl;   
+  }
+}
+    
